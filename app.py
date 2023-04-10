87831a00f7c2e1025dc7f091283fc50d63483051
Haihan Wu <haihanwu@mit.edu>
2023-04-10 13:51:53 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import openai
+import os
+from text.functions import summarize
+
+try:
+	openai.api_key = os.getenv('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')
+
+	st.title("Text Summarizer")
+
+	# initialize state variable
+	if "summary" not in st.session_state:
+	    st.session_state["summary"] = ""
+
+	input_text = st.text_area(label='Enter full text:', value="", height=250)
+
+	st.button(
+	    "submit".
+	    on_click=summarize,
+	    kwargs={"prompt": input_text}
+	)
+
+	# submit botton is pressed and generate the summary, state becomes "summary"
+	output_text = st.text_area(label='Summarized text:', value=st.session_state["summary"], height=250)
+except:
+	st.write('There was an error =(')
+
+# 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
